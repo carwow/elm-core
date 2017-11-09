4044460989a703e6974410fa7170315fae8549b3
Muyiwa Olu <signups@muyiwa.me>
2017-11-09 14:29:45 +0000

    Minor syntax tweaks

--- src/Core/Notifier.elm
+++ src/Core/Notifier.elm
@@ -1,4 +1,4 @@
-module Core.Notifier exposing (subscriptions, view, Model, update, Msg(NewNotification), init, Flags, Msg(DrawerMsg))
+module Core.Notifier exposing (subscriptions, view, Model, update, Msg(NewNotification, DrawerMsg), init, Flags)
 
 {-| Displays latest notifications
 
